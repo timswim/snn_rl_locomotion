17294b704c36559db3f8aa749607c31e13df2393
isakov.tim <timswim1337@gmail.com>
2026-01-21 00:09:11 +0300

    Добавил cartpole среду

---
+++ source/locomotion/locomotion/tasks/manager_based/classic/cartpole/__init__.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2022-2026, The Isaac Lab Project Developers (https://github.com/isaac-sim/IsaacLab/blob/main/CONTRIBUTORS.md).
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""
+Cartpole balancing environment.
+"""
+
+import gymnasium as gym
+
+from . import agents
+
+##
+# Register Gym environments.
+##
+
+gym.register(
+    id="Isaac-Cartpole-v0",
+    entry_point="isaaclab.envs:ManagerBasedRLEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.cartpole_env_cfg:CartpoleEnvCfg",
+    },
+)
